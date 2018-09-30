379b84db4cd8bb9a2390208123cdfbaa97525a78
btzy <bernard.14916@gmail.com>
2018-09-30 15:06:54 +0800

    Add new deadline field to Task

---
+++ src/main/java/seedu/address/model/task/Deadline.java
@@ -0,0 +1,62 @@
+package seedu.address.model.task;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents a Task's deadline in the deadline manager. Guarantees: immutable; represents a valid date
+ */
+public class Deadline {
+
+    public static final String MESSAGE_DEADLINE_CONSTRAINTS =
+            "Deadline has to be a valid date";
+
+    public final Date value;
+
+    /**
+     * Constructs an {@code Address}.
+     *
+     * @param deadline A valid deadline.
+     */
+    public Deadline(Date deadline) {
+        requireNonNull(deadline);
+        this.value = deadline;
+    }
+
+    /**
+     * Constructs an {@code Address}.
+     *
+     * @param deadline A valid deadline.
+     */
+    public Deadline(String deadline) {
+        requireNonNull(deadline);
+
+        DateFormat formatter = DateFormat.getInstance();
+        try {
+            this.value = formatter.parse(deadline);
+        } catch (ParseException e) {
+            throw new IllegalArgumentException(MESSAGE_DEADLINE_CONSTRAINTS, e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        DateFormat formatter = DateFormat.getInstance();
+        return formatter.format(value);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Deadline // instanceof handles nulls
+                && value.equals(((Deadline) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
