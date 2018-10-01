6c70fb2517381e540f7de5517bdfb2aabe0ebf5a
Bernard Teo <btzy1996@hotmail.com>
2018-10-01 14:54:53 +0800

    Filter: Fix code styles

--- src/main/java/seedu/address/model/task/FilterOperator.java
+++ src/main/java/seedu/address/model/task/FilterOperator.java
@@ -2,24 +2,32 @@ package seedu.address.model.task;
 
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Represents an operator used for the filter predicate.
+ */
 public enum FilterOperator {
     CONVENIENCE,
     EQUAL,
     LESS,
     GREATER;
 
+    /**
+     * Parses a string into a filter operator.
+     *
+     * @param str The string to parse.
+     */
     public static FilterOperator parse(String str) throws ParseException {
-        switch(str){
-            case ":":
-                return CONVENIENCE;
-            case "=":
-                return EQUAL;
-            case "<":
-                return LESS;
-            case ">":
-                return GREATER;
-            default:
-                throw new ParseException("Invalid filter operator!");
+        switch(str) {
+        case ":":
+            return CONVENIENCE;
+        case "=":
+            return EQUAL;
+        case "<":
+            return LESS;
+        case ">":
+            return GREATER;
+        default:
+            throw new ParseException("Invalid filter operator!");
         }
     }
 }
