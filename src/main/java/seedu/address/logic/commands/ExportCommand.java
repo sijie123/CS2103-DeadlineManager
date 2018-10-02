75e279ec81855bf1e4e5d75d773ea915bfcef131
sijie123 <sijie123@gmail.com>
2018-10-02 17:00:36 +0800

    Fix import style-check issues.

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -1,21 +1,18 @@
 package seedu.address.logic.commands;
 
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.exceptions.DataConversionException;
-import seedu.address.logic.CommandHistory;
-import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.Model;
-import seedu.address.model.ReadOnlyTaskCollection;
-import seedu.address.model.TaskCollection;
-import seedu.address.model.task.Task;
-import seedu.address.storage.Storage;
+import static java.util.Objects.requireNonNull;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-import static java.util.Objects.requireNonNull;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.TaskCollection;
+import seedu.address.model.task.Task;
+import seedu.address.storage.Storage;
 
 /**
  * Imports all tasks from an external deadline manager export file.
