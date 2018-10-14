c07ff0c0544e905920086c8c533617b9026d4762
btzy <bernard.14916@gmail.com>
2018-10-14 11:51:00 +0800

    Fix style issues

--- src/main/java/seedu/address/logic/parser/exceptions/BooleanExpressionParseException.java
+++ src/main/java/seedu/address/logic/parser/exceptions/BooleanExpressionParseException.java
@@ -1,5 +1,9 @@
 package seedu.address.logic.parser.exceptions;
 
+/**
+ * Represents an exception that is caused by parsing a boolean expression.
+ * This exception is thrown by methods in BooleanExpressionParser.
+ */
 public class BooleanExpressionParseException extends ParseException {
     public BooleanExpressionParseException(String message) {
         super(message);
