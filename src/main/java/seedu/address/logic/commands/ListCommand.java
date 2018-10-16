71097afd076539ceaac192f25cec3a9e4946537b
ranaldmiao <ranaldmiao@gmail.com>
2018-10-17 01:31:23 +0800

    Fix ListCommand

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -7,13 +7,13 @@ import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 
 /**
- * Lists all persons in the deadline manager to the user.
+ * Lists all tasks in the deadline manager to the user.
  */
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_SUCCESS = "Listed all tasks";
 
 
     @Override
