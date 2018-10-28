53ae678823af857592e75f9e72cf01a7476c3c72
ranaldmiao <ranaldmiao@gmail.com>
2018-10-29 02:32:57 +0800

    Remove redundant string constant from AttachmentCommand

--- src/main/java/seedu/address/logic/commands/AttachmentCommand.java
+++ src/main/java/seedu/address/logic/commands/AttachmentCommand.java
@@ -50,7 +50,6 @@ public class AttachmentCommand extends Command {
 
 
     public static final String MESSAGE_MISSING_ARGUMENTS = "Missing argument %1$s for %2$s action";
-    public static final String MESSAGE_DUPLICATED_ARGUMENTS = "Only one argument of %1$s allowed";
     public static final String MESSAGE_NAME_NOT_FOUND = "%1$s is not an attachment.";
 
     private static final Logger logger = LogsCenter.getLogger(Attachment.class);
