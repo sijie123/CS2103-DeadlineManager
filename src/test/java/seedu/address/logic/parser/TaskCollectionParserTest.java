b277b3709781a59c4e6c6abfd05b6a0da2dda8b5
btzy <bernard.14916@gmail.com>
2018-10-28 23:16:27 +0800

    Remove unused imports

--- src/test/java/seedu/address/logic/parser/TaskCollectionParserTest.java
+++ src/test/java/seedu/address/logic/parser/TaskCollectionParserTest.java
@@ -6,10 +6,6 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_TASK;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
