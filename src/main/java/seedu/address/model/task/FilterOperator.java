94c9da8082a8bcb05f95d93201c4586ea488c1c7
btzy <bernard.14916@gmail.com>
2018-11-03 23:51:59 +0800

    Remove unused import

--- src/main/java/seedu/address/model/task/FilterOperator.java
+++ src/main/java/seedu/address/model/task/FilterOperator.java
@@ -1,6 +1,5 @@
 package seedu.address.model.task;
 
-import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.task.exceptions.InvalidPredicateOperatorException;
 
 /**
