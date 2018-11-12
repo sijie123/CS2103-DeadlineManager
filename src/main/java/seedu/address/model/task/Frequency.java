dc9fd36111a422314be1b459d1930a39644ac67d
btzy <btzy1996@hotmail.com>
2018-11-12 17:34:05 +0800

    Fix frequency error message to say "integer" not "number"

--- src/main/java/seedu/address/model/task/Frequency.java
+++ src/main/java/seedu/address/model/task/Frequency.java
@@ -16,7 +16,7 @@ public class Frequency implements Comparable<Frequency> {
 
 
     public static final String MESSAGE_FREQUENCY_CONSTRAINTS =
-        "Frequency should only be a non negative number";
+        "Frequency should only be a non-negative integer";
     public static final String FREQUENCY_VALIDATION_REGEX = "[0-9]+";
     public static final String NO_FREQUENCY = "0";
     public final int value;
